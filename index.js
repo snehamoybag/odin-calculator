1b11fd8dcd6da1dc266ae59735149c2979d69b05
snehamoybag <snehamoybag2@gmail.com>
2023-08-25 23:24:34 +0530

    make operator buttons work properly
    if user presses operator button consecutively, replace the old operator
      with new one.

--- index.js
+++ index.js
@@ -2,16 +2,23 @@ function DisplayConstructor() {
   const equation = document.querySelector("#display-equation");
   const result = document.querySelector("#display-result");
 
-  this.updateEquation = (value) => {
-    equation.value = value === "" ? value : equation.value + value;
-  };
   this.getEquation = () => equation.value;
+  this.setEquation = (value) => (equation.value = value);
+  this.updateEquation = (value) => (equation.value = equation.value + value);
 
-  this.updateResult = (value) => (result.value = value);
   this.getResult = () => result.value;
+  this.updateResult = (value) => (result.value = value);
 }
 
 const display = new DisplayConstructor();
+
+const buttons = {
+  numbers: document.querySelectorAll("button[data-type=number]"),
+  operators: document.querySelectorAll("button[data-type=operator]"),
+  equal: document.querySelector("#equal"),
+  undo: document.querySelector("#undo"),
+  clear: document.querySelector("#clear"),
+};
 
 const add = function(num1, num2) {
   return num1 + num2;
@@ -52,28 +59,30 @@ const operate = function(num1, num2, operator) {
   return result;
 };
 
-const buttons = {
-  numbers: document.querySelectorAll("button[data-type=number]"),
-  operators: document.querySelectorAll("button[data-type=operator]"),
-  equal: document.querySelector("#equal"),
-  undo: document.querySelector("#undo"),
-  clear: document.querySelector("#clear"),
-};
-
 // update display equation on button clicks
 buttons.numbers.forEach((button) => {
   button.addEventListener("click", () => display.updateEquation(button.value));
 });
 
 buttons.operators.forEach((button) => {
-  button.addEventListener("click", () => display.updateEquation(button.value));
+  button.addEventListener("click", () => {
+    const currentEquation = display.getEquation();
+    const nonOperatorReg = /[0-9\.]/;
+
+    // replace operator if user press operator buttons consecutively
+    if (!nonOperatorReg.test(currentEquation[currentEquation.length - 1])) {
+      const updatedEquation = currentEquation.slice(0, -1).concat(button.value);
+      display.setEquation(updatedEquation);
+    } else {
+      display.updateEquation(button.value);
+    }
+  });
 });
 
 buttons.undo.addEventListener("click", () => {
   const currentEquation = display.getEquation();
   const prevEquation = currentEquation.slice(0, -1); // remove last item from equation
-  display.updateEquation(""); // clear everything
-  display.updateEquation(prevEquation); // sets it to previous equation
+  display.setEquation(prevEquation);
 });
 
-buttons.clear.addEventListener("click", () => display.updateEquation(""));
+buttons.clear.addEventListener("click", () => display.setEquation(""));
